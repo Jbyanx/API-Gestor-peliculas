c68d4df15ce9b881f54c2a20f5f714ea50c8fa8e
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-07-01 15:36:00 -0500

    Creacion de Capa de Acceso a Bases de Datos. - MovieCrudRepository.java - UserCrudRepository.java - RatingCrudRepository.java
    Se agrego la enumeracion siguiente
    - MovieGenre.java

---
+++ src/main/java/com/bycompany/projects/MovieManager/persistence/repository/UserCrudRepository.java
@@ -0,0 +1,25 @@
+package com.bycompany.projects.MovieManager.persistence.repository;
+
+import com.bycompany.projects.MovieManager.persistence.entity.Rating;
+import com.bycompany.projects.MovieManager.persistence.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserCrudRepository extends JpaRepository<User, Long> {
+
+    List<User> findAllByNameContaining(String name);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> updateByUsername(String username);
+
+    @Modifying
+    void deleteByUsername(String username);
+
+    @Query("SELECT u FROM User JOIN ")
+    List<Rating> findRatingsForUserByUsername(String username);
+}
